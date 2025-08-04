d3f432bcc6f2539729b7a3287f7e24b77e3f3e85
prnvgithub28 <sanaspranav28@gmail.com>
2025-08-04 19:29:05 +0530

    added dashboard card component

--- frontend/src/Dashboard/Dashboard.jsx
+++ frontend/src/Dashboard/Dashboard.jsx
@@ -1,11 +1,58 @@
-import React from 'react'
+import Dashboard_Card from "./Dashboard_Card";
+import Welcome_Card from "./Welcome_Card";
+import Calendar from "./Calender";
+import Student_Table from "./Student_Table";
+import { useGlobalContext } from "../Context/GlobalContext";
+import { useEffect } from "react";
+import Teacher_Table from "./Teacher_Table";
 
-function Dashboard() {
+const Dashboard = () => {
+  const { fetchStudents, fetchTeachers, students, teachers } =
+    useGlobalContext();
+
+  useEffect(() => {
+    fetchStudents();
+    fetchTeachers();
+  }, []);
+
   return (
-    <div>
-      <p>Dashboard</p>
+    <div className="flex " style={{ height: "calc(100vh - 104px)" }}>
+      <div className="w-3/5 border-r-4 mr-1">
+        <div className="flex">
+          <div className="m-2">
+            <Welcome_Card />
+          </div>
+          <div className="flex">
+            <div className="m-2">
+              <Dashboard_Card
+                img="https://static.vecteezy.com/system/resources/previews/008/154/360/non_2x/student-logo-vector.jpg"
+                topic={"Student"}
+                totalCount={students.length}
+              />
+            </div>
+            <div className="m-2">
+              <Dashboard_Card
+                img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUgPuFMoqcbT9HuTtsUrujn57xjYciwbmVFA&s"
+                topic={"Teacher"}
+                totalCount={teachers.length}
+              />
+            </div>
+          </div>
+        </div>
+        <div className="mt-4">
+          <Student_Table />
+        </div>
+      </div>
+      <div className="calender  w-2/5">
+        <div>
+          <Calendar />
+        </div>
+        <div className="mt-4">
+          <Teacher_Table teachers={teachers} />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
--- frontend/src/Dashboard/Dashboard_Card.jsx
+++ frontend/src/Dashboard/Dashboard_Card.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+/* eslint-disable react/prop-types */
 
-function Dashboard_Card() {
+const Dashboard_Card = ({ img, totalCount, topic }) => {
   return (
-    <div>
-      <p>Dashboard_Card</p>
-    </div>
-  )
-}
+    <div className="w-56 h-48 bg-gray-100 outline rounded-sm outline-green-950">
+      <div className="h-4/5 flex  justify-evenly align-middle">
+        <div className="h-full w-full">
+          <img
+            src={img}
+            alt="student logo"
+            className="rounded-full h-full "
+          /> 
+        </div>
 
-export default Dashboard_Card
+        <p className="text-xl px-2">{totalCount}</p>
+      </div>
+      <div className="h-1/5 flex flex-col justify-between align-middle">
+        <div className="bg-black"></div>
+        <h1 className="text-xl text-center">{topic}</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard_Card;
