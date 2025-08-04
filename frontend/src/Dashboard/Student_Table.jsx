2db65bfc01c114e73656cac0bc5e337391da3377
prnvgithub28 <sanaspranav28@gmail.com>
2025-08-04 19:41:44 +0530

    added student, teacher table and welcome card

--- frontend/src/Dashboard/Student_Table.jsx
+++ frontend/src/Dashboard/Student_Table.jsx
@@ -1,11 +1,94 @@
-import React from 'react'
+import { useEffect } from "react";
+import { useGlobalContext } from "../Context/GlobalContext";
 
-function Student_Table() {
+const Student_Table = () => {
+  const { fetchStudents, students, setStudents } = useGlobalContext();
+
+  useEffect(() => {
+    fetchStudents();
+  }, []);
+
+  const deleteStudent = (id) => {
+    setStudents((prevStudent) =>
+      prevStudent.filter((student) => student._id !== id)
+    );
+  };
   return (
-    <div>
-      <p>Student_Table</p>
+    <div className="flex flex-col">
+      <div className="-m-1.5 overflow-x-auto">
+        <div className="p-1.5 min-w-full inline-block align-middle">
+          <h1 className="text-2xl font-semibold">Students</h1>
+          <div className="overflow-hidden">
+            <table className="min-w-full divide-y divide-gray-200">
+              <thead>
+                <tr>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase"
+                  >
+                    S No.
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase"
+                  >
+                    Name
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase"
+                  >
+                    Email
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase"
+                  >
+                    Address
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-end text-xs font-medium text-gray-500 uppercase"
+                  >
+                    Action
+                  </th>
+                </tr>
+              </thead>
+              <tbody className="divide-y divide-gray-200">
+                {students?.map((student, index) => (
+                  <tr key={student._id}>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800">
+                      {index + 1}
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800">
+                      {student.fullname}
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800">
+                      {student.email}
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800">
+                      New York No. {index + 1} Lake Park
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap text-end text-sm font-medium">
+                      <button
+                        type="button"
+                        className="inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent text-blue-600 hover:text-blue-800 focus:outline-none focus:text-blue-800 disabled:opacity-50 disabled:pointer-events-none"
+                        onClick={() => {
+                          deleteStudent(student._id);
+                        }}
+                      >
+                        Delete
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Student_Table
+export default Student_Table;
