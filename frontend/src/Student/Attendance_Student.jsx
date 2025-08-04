ab61a18b7da026d74c312774d9743d2e895493ea
prnvgithub28 <sanaspranav28@gmail.com>
2025-08-04 19:38:47 +0530

    made student and teacher file str

---
+++ frontend/src/Student/Attendance_Student.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Attendance_Student() {
+  return (
+    <div>
+      <p>hello</p>
+    </div>
+  )
+}
+
+export default Attendance_Student
---
+++ frontend/src/Teacher/AddNewsModal.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AddNewsModal() {
+  return (
+    <div>
+      <p>naman bsdk</p>
+    </div>
+  )
+}
+
+export default AddNewsModal
